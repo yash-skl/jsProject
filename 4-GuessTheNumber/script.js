cfb1242dd197754787c4d97d17f55cff07eac023
yash-skl <yshukla2775@gmail.com>
2025-05-20 10:46:48 +0530

    created number guess

---
+++ 4-GuessTheNumber/script.js
@@ -0,0 +1,49 @@
+const numberGuess = Math.floor(Math.random() * 100);
+const form = document.querySelector('form')
+const subt = document.querySelector('#subt');
+const lowOrHi = document.querySelector('.lowOrHi')
+const guessField = document.querySelector('#guessField')
+const lastResult = document.querySelector('.lastResult')
+const guesses = document.querySelector('.guesses')
+const lottieContainer = document.getElementById('lottie-container');
+// lottieContainer.style.display = 'none'; 
+let guessesArray = [];
+
+const animation = lottie.loadAnimation({
+    container: lottieContainer,
+    renderer: 'svg',
+    loop: true,
+    autoplay: false, 
+    path: 'https://raw.githubusercontent.com/yash-skl/lottie-animations/refs/heads/main/celebration.json' // correct JSON link
+  });
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const userGuess = Number(guessField.value);
+    let guessRemaining = Number(lastResult.textContent)
+
+    if (!userGuess || userGuess < 1 || userGuess > 100) {
+        lowOrHi.textContent = "Please enter a valid number between 1 and 100.";
+        return;
+      }
+    if (userGuess == numberGuess) {
+        lowOrHi.textContent = 'Correct Answer! 🎉';
+        lottieContainer.style.display = 'block';
+        animation.goToAndPlay(0, true);
+        subt.disabled = true;
+    } else {
+        guessRemaining = guessRemaining - 1;
+        lastResult.textContent = guessRemaining;
+        guessesArray.push(userGuess);
+        guesses.textContent = guessesArray.join(', ');
+        lowOrHi.innerHTML = userGuess > numberGuess ? "Too High!" : "Too Low!";
+        if (guessRemaining == 0) {
+            lastResult.textContent = 'Your guesses are over.'
+            subt.disabled = true;
+        }
+    }
+
+    guessField.value = '';
+    guessField.focus();
+})
